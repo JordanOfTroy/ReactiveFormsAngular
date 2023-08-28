9cde3199580ab5bcbc6fa6c496c4654e7d809597
Jordan <jordan@Jordans-MacBook-Air.local>
2023-08-28 08:55:13 -0600

    added form validation

--- src/app/customer-contact-form/customer-contact-form.component.ts
+++ src/app/customer-contact-form/customer-contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, FormGroupDirective, FormArray, Validators, ValidatorFn, AbstractControl, ValidationErrors} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-customer-contact-form',
@@ -10,11 +11,13 @@ export class CustomerContactFormComponent {
 
   form: FormGroup;
   fb: FormBuilder = new FormBuilder;  // we'll want to be able to access this later outside of the controller
+  customErrorStateMatcher: ErrorStateMatcher = new CustomErrorStateMatcher();
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
-      firstName: [ 'Henry' ],
-      lastName: [ 'Cavill' ],
+      firstName: [ 'Henry', Validators.compose([ Validators.required, this.forbiddenNameValidator() ]) ],
+      lastName: [ 'Cavill', Validators.required ],
+      email: ['Henry@Cavill.com', Validators.compose([ Validators.required, Validators.email ])],
       phoneNumbers: fb.array([
         fb.group({
         alias: ['Home'],
@@ -35,12 +38,13 @@ export class CustomerContactFormComponent {
         state: [''],
         zip: ['']
       })
-    });
+    }, { validator: this.forbiddenFullNameValidator })
   }
 
   reset(): void {
     this.form.controls['firstName'].setValue('');
     this.form.controls['lastName'].setValue('');
+    this.form.controls['email'].setValue('');
   }
 
   fillDefaultAddress(): void {
@@ -80,6 +84,31 @@ export class CustomerContactFormComponent {
 
   resetAll(): void {
     this.form.reset()
+  }
+
+  // Custom Validator
+  forbiddenNameValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const forbidden = new RegExp(/donald/i).test(control.value);
+      return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+    };
+  }
+  
+  //cross field validation
+  forbiddenFullNameValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+    const firstName = control.get('firstName').value;
+    const lastName = control.get('lastName').value;
+    const fullName = `${ firstName } ${ lastName }`;
+    const forbidden = new RegExp(/^mickey mouse$/i).test(fullName);
+    return forbidden ? { 'forbiddenFullName': { value: fullName } } : null;
+  }
+
+}
+
+class CustomErrorStateMatcher implements ErrorStateMatcher {
+
+  isErrorState(control: FormControl, form: FormGroupDirective): boolean {
+    return form.getError('forbiddenFullName') || control.invalid;
   }
 
 }
